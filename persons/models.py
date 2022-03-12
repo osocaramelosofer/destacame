3c3c399fe05e3ebe99de8c120a9db37ad2554815
osocaramelosofer <osocaramelosofer@hotmail.com>
2022-03-11 21:55:03 -0600

    Created Passenger & Driver models

--- persons/models.py
+++ persons/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Passenger(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+
+
+class Driver(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
