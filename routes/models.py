0dd2ad57480d5f4d0937b3fdd9520ed7f08e50d3
osocaramelosofer <osocaramelosofer@hotmail.com>
2022-03-11 21:46:54 -0600

    Applied Route migration

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Route(models.Model):
+    origin = models.CharField(max_length=50, blank=True, null=True)
+    destination = models.CharField(max_length=50, blank=True, null=True)
