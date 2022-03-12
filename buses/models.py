888603ba13961fd21f400fb06d187399f05e588a
osocaramelosofer <osocaramelosofer@hotmail.com>
2022-03-11 21:41:23 -0600

    Applied buss migration

--- buses/models.py
+++ buses/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Buss(models.Model):
+    plate = models.CharField(max_length=12, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
