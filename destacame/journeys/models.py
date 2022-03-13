63801ba5410139f5117ef0f2b700a943e1be5fed
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 09:55:39 -0600

    Added relationship Journery with buss

--- destacame/journeys/models.py
+++ destacame/journeys/models.py
@@ -1,14 +1,21 @@
+from django.utils.translation import gettext_lazy as _
 
 from django.db import models
+# from destacame.destacame.buses.models import Buss2
 from ..buses.models import Buss
-from ..routes.models import Route
-from ..persons.models import Passenger, Driver
+# from ..routes.models import Route
+# from destacame.buses.models import Buss2
+# from ..persons.models import Passenger, Driver
 
 # Create your models here.
 class Journey(models.Model):
-    buss = models.ForeignKey('Buss', on_delete=models.CASCADE)
-    route = models.ForeignKey('Route', on_delete=models.CASCADE)
-    passenger = models.ForeignKey('Passenger', on_delete=models.CASCADE)
-    driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
-    date = models.DateField()
+    # buss = models.IntegerField()
+    route = models.IntegerField()
+    passenger = models.IntegerField()
+
+    buss = models.ForeignKey('buses.Buss', on_delete=models.CASCADE)
+    # route = models.ForeignKey('Route', on_delete=models.CASCADE)
+    # passenger = models.ForeignKey('Passenger', on_delete=models.CASCADE)
+    # driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
+    # date = models.DateField()
 
