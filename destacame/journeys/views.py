755e5ea774dabc6327f483a1faa3ef063b78e295
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 14:12:57 -0600

    Create ListView to list journeys

--- destacame/journeys/views.py
+++ destacame/journeys/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Journey
 
 # Create your views here.
+class ListJourneys(ListView):
+    model = Journey
+    template_name = 'pages/journeys.html'
+    context_object_name = 'journeys'
