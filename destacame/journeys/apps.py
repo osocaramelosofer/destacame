91e385acd441d62409163670f7708ab137e181d3
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 09:55:12 -0600

    Configured JourneysConfig

--- destacame/journeys/apps.py
+++ destacame/journeys/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class JourneysConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'journeys'
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = 'destacame.journeys'
+    verbose_name = _("Journeys")
+
+    def ready(self):
+        try:
+            pass
+        except ImportError:
+            pass
