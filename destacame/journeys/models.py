a8f6996bf534482efb5c72cde2dece7799a0f9ba
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 10:29:00 -0600

    Added passenger & driver relathionship with journey

--- destacame/journeys/models.py
+++ destacame/journeys/models.py
@@ -10,7 +10,7 @@ from ..persons.models import Passenger, Driver
 class Journey(models.Model):
     buss = models.ForeignKey('buses.Buss', on_delete=models.CASCADE)
     route = models.ForeignKey('routes.Route', on_delete=models.CASCADE)
-    passenger = models.ForeignKey('persons.Passenger', on_delete=models.CASCADE)
-    driver = models.ForeignKey('persons.Driver', on_delete=models.CASCADE)
+    passenger = models.ForeignKey('persons.Passenger', on_delete=models.CASCADE, null=True, blank=True)
+    driver = models.ForeignKey('persons.Driver', on_delete=models.CASCADE, null=True)
     date = models.DateField(null=True, blank=True)
 
