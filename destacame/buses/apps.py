40f242acb48cd4f8d1952368effff18f0fbddf5f
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 09:53:23 -0600

    Configured BusesConfig

--- destacame/buses/apps.py
+++ destacame/buses/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class BusesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'buses'
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = "destacame.buses"
+    verbose_name = _("Buses")
+
+    def ready(self):
+        try:
+            pass
+        except ImportError:
+            pass
