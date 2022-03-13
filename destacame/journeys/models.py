85d27edc32c3ede5c600b4483fdee4403590e997
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 10:16:49 -0600

    Added passenger & driver admin

--- destacame/journeys/models.py
+++ destacame/journeys/models.py
@@ -1,18 +1,14 @@
-
 from django.db import models
-# from destacame.destacame.buses.models import Buss2
+
 from ..buses.models import Buss
 from ..routes.models import Route
-# from destacame.buses.models import Buss2
-# from ..persons.models import Passenger, Driver
+from ..persons.models import Passenger, Driver
 
 # Create your models here.
 class Journey(models.Model):
-    passenger = models.IntegerField()
-
     buss = models.ForeignKey('buses.Buss', on_delete=models.CASCADE)
     route = models.ForeignKey('routes.Route', on_delete=models.CASCADE)
-    # passenger = models.ForeignKey('Passenger', on_delete=models.CASCADE)
-    # driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
-    # date = models.DateField()
+    passenger = models.ForeignKey('persons.Passenger', on_delete=models.CASCADE)
+    driver = models.ForeignKey('persons.Driver', on_delete=models.CASCADE)
+    date = models.DateField(null=True, blank=True)
 
--- destacame/persons/admin.py
+++ destacame/persons/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Driver, Passenger
 
 # Register your models here.
+admin.site.register(Passenger)
+admin.site.register(Driver)
