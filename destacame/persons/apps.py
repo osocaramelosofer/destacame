5d8adb36455d4e2d251c0908292828265e544f4c
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 09:56:16 -0600

    Configured PersonsConfig

--- destacame/persons/apps.py
+++ destacame/persons/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class PersonsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'persons'
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = 'destacame.persons'
+    verbose_name = _("Persons")
+
+    def ready(self):
+        try:
+            pass
+        except ImportError:
+            pass
