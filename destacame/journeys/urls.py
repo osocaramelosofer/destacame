d79ffddc80eb9a7cfc88d7c82343d17dd1034ba6
Fernando <osocaramelosofer@hotmail.com>
2022-03-13 21:46:55 -0600

    Added journey detail

--- destacame/journeys/urls.py
+++ destacame/journeys/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ListJourneys
+from .views import ListJourneys, JourneyDetail
+
 app_name = "journeys"
 urlpatterns = [
     path("list/", view=ListJourneys.as_view(), name="list"),
+    path("detail/<int:pk>", view=JourneyDetail.as_view(), name="journey_detail"),
 ]
--- destacame/journeys/views.py
+++ destacame/journeys/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-
+from django.views.generic import ListView, UpdateView, DetailView
+from django.urls import reverse_lazy
 
 from .models import Journey
 
@@ -9,3 +9,15 @@ class ListJourneys(ListView):
     model = Journey
     template_name = 'pages/journeys.html'
     context_object_name = 'journeys'
+
+
+class JourneyDetail(DetailView):
+    model = Journey
+    template_name = 'pages/journeys/detail.html'
+
+
+# class UpdateJourney(UpdateView):
+#     model = Journey
+#     template_name = ''
+#     form_class = JourneyForm
+#     success_url = reverse_lazy('')
