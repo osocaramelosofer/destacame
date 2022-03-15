fee3782f2c0eb310aeb47d55d031442d9aed443c
Fernando <osocaramelosofer@hotmail.com>
2022-03-14 21:51:23 -0600

    Added CRUD views for Passenger

--- destacame/persons/views.py
+++ destacame/persons/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 
 from .models import Passenger, Driver
 
@@ -8,6 +9,18 @@ class ListPassengers(ListView):
     model = Passenger
     context_object_name = 'passengers'
     template_name = 'pages/passengers/list.html'
+
+
+class ListDrivers(ListView):
+    model = Driver
+    context_object_name = 'drivers'
+    template_name = 'pages/drivers/list.html'
+
+
+class PassengerCreateView(CreateView):
+    model = Passenger
+    template_name = 'pages/passengers/create.html'
+    fields = ['name']
 
 
 def create_passenger(request):
@@ -21,7 +34,18 @@ def create_passenger(request):
     return render(request, 'pages/passengers/create.html')
 
 
-class ListDrivers(ListView):
-    model = Driver
-    context_object_name = 'drivers'
-    template_name = 'pages/drivers/list.html'
+class PassengerDetail(DetailView):
+    model = Passenger
+    template_name = 'pages/passengers/detail.html'
+
+
+class PassengerEdit(UpdateView):
+    model = Passenger
+    template_name = 'pages/passengers/edit_passenger.html'
+    fields = ['name']
+
+
+# class PassengerDelete(DeleteView):
+#     model = Passenger
+#     template_name = 'pages/passengers/delete.html'
+#     success_url = 'persons:list-passengers'
