156b53d1e7b8c88b9d9d805a89734aae8d995a64
Fernando <osocaramelosofer@hotmail.com>
2022-03-15 10:26:11 -0600

    finished crud for drivers

--- destacame/buses/urls.py
+++ destacame/buses/urls.py
@@ -1,7 +1,14 @@
-from .views import ListBuses
 from django.urls import path
+
+from .views import ListBuses, CreateBuss, DetailBuss, EditBuss, DeleteBuss
+
 
 app_name = "buses"
 urlpatterns = [
     path("list/", view=ListBuses.as_view(), name="list"),
+    path("craete/", view=CreateBuss.as_view(), name="create_buss"),
+    path("detail/<int:pk>", view=DetailBuss.as_view(), name="buss_detail"),
+    path("edit/<int:pk>", view=EditBuss.as_view(), name="edit_buss"),
+    path("delete/<int:pk>", view=DeleteBuss.as_view(), name="delete_buss")
+
 ]
--- destacame/buses/views.py
+++ destacame/buses/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Buss
+
 
 class ListBuses(ListView):
     model = Buss
     context_object_name = 'buses'
     template_name = 'pages/buses/list.html'
+
+
+class CreateBuss(CreateView):
+    model = Buss
+    template_name = 'pages/buses/create.html'
+    fields = ['plate', 'name']
+
+
+class DetailBuss(DetailView):
+    model = Buss
+    template_name = 'pages/buses/detail.html'
+
+
+class EditBuss(UpdateView):
+    model = Buss
+    template_name = 'pages/buses/edit.html'
+    fields = ['plate', 'name']
+
+
+class DeleteBuss(DeleteView):
+    model = Buss
+    template_name = 'pages/buses/delete.html'
+    success_url = reverse_lazy('buses:list')
